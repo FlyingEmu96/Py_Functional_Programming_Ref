a47c15319cb0ca0bdd4070ca92a6f2db464d36d4
Emir_Flying_Emu96 <emir.murat1337@gmail.com>
2025-09-03 14:29:27 +0100

    Add function to pair documents with valid formats

---
+++ example_10.py
@@ -0,0 +1,14 @@
+valid_formats = [
+    "docx",
+    "pdf",
+    "txt",
+    "pptx",
+    "ppt",
+    "md",
+]
+
+# Don't edit above this line
+
+def pair_document_with_format(doc_names, doc_formats):
+    valid = list(zip(doc_names, doc_formats))
+    return list(filter(lambda x: x[1] in valid_formats, valid))
